29f96f3c61f5871edf86dccc9388929219f937e5
shilongxiang <jinxiseverything@gmail.com>
2025-02-22 17:58:11 +0000

    Deploying to gh-pages from @ shilongxiang/shilongxiang.github.io@b4bf49df86578a4f47e3c9198c103b16f61dd8f2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -361,16 +361,8 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+    },{id: "news-i-received-the-muraj-in-action-grant-award-smile",
+          title: 'I received the MURAJ In-Action Grant Award.:smile:',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
